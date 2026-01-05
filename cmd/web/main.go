c896ad3c6dc13b94ee9720f9fcdccc10f6742c16
Fernando Cañas <fernando.canas@outlook.com>
2026-01-05 16:44:00 -0500

    feat: use env variables to handle HTTP port

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 	"os"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP Network address")
+	flag.Parse()
+
 	mux := http.NewServeMux()
-	
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-	
+
 	mux.HandleFunc("GET /{$}", home)
 	mux.HandleFunc("GET /snippet/view/{id}", snippetView)
 	mux.HandleFunc("GET /snippet/create", snippetCreate)
 	mux.HandleFunc("POST /snippet/create", snippetCreatePost)
 
-	log.Print("Starting server on :4000")
+	log.Println("Starting server on", *addr)
 
-	err := http.ListenAndServe(":4000", mux)
+	err := http.ListenAndServe(*addr, mux)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
