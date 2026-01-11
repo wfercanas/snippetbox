7ee39dce9d792fd1d24441b138c5942e58c15435
Fernando Cañas <fernando.canas@outlook.com>
2026-01-10 19:37:30 -0500

    feat: serverError and clientError helpers

---
+++ cmd/web/helpers.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *application) serverError(w http.ResponseWriter, r *http.Request, err error) {
+	method := r.Method
+	uri := r.URL.RequestURI()
+	// trace := string(debug.Stack())
+	// app.logger.Error(err.Error(), "method", method, "uri", uri, "trace", trace)
+
+	app.logger.Error(err.Error(), "method", method, "uri", uri)
+	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+}
+
+func (app *application) clientError(w http.ResponseWriter, status int) {
+	http.Error(w, http.StatusText(status), status)
+}
