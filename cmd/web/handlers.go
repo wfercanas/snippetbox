220e4ccc263375538699c2f9aab4d74e422bded9
Fernando Cañas <fernando.canas@outlook.com>
2026-01-22 17:37:40 -0500

    feat: insert new snippet using handler and snippet model insert query

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -44,6 +44,16 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte("Saved a new snippet..."))
+	title := "O snail"
+	content := "O snail\nClimb Mount Fuji,\nBut slowly, slowly!\n\n– Kobayashi Issa"
+	expires := 7
+
+	id, err := app.snippets.Insert(title, content, expires)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
+	fmt.Println("Redirect")
+	http.Redirect(w, r, fmt.Sprintf("/snippet/view/%d", id), http.StatusSeeOther)
 }
