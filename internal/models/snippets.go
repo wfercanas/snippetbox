99d4af943e61b84dcdd7850695e26cb373a08ca4
Fernando Cañas <fernando.canas@outlook.com>
2026-01-23 17:49:02 -0500

    feat: get latest snippets through home handler and latest model method

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -56,5 +56,34 @@ func (m *SnippetModel) Get(id int) (Snippet, error) {
 }
 
 func (m *SnippetModel) Latest() ([]Snippet, error) {
-	return nil, nil
+	stmt := `SELECT id, title, content, created, expires
+	FROM snippets
+	WHERE expires > UTC_TIMESTAMP()
+	ORDER BY id
+	LIMIT 10`
+
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var snippets []Snippet
+	for rows.Next() {
+		var s Snippet
+
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+
+		snippets = append(snippets, s)
+	}
+
+	err = rows.Err()
+	if err != nil {
+		return nil, err
+	}
+
+	return snippets, nil
 }
